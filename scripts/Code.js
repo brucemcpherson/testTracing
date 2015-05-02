7b2a0e599fa06c67a20df06ce0e1fd1739067c49
Bruce McPherson <bruce@mcpher.com>
2015-05-02 23:02:56 +0100

    updated by GasGit automation

---
+++ scripts/Code.js
@@ -0,0 +1,42 @@
+
+function myFunction() {
+  
+  // -- always leave this comment somewhere .. its a hack to provoke an authorization dialog ... Drive.Files.copy(resource, fileId)
+  
+  // create tracing object - need to have enabled drive advanced service to get a properly scoped token
+  var trace = new cChromeTrace.ChromeTrace().setAccessToken(ScriptApp.getOAuthToken());
+  
+  var text = "abcdefghijklmnop";
+  var key = "xyz";
+  
+  // add a couple of events
+  trace.begin('hmac');
+  
+    var LOOPSIZE = 1000;
+  
+    trace.begin('hmac256');
+    
+      for (var i = 0 ; i < LOOPSIZE ; i++) {
+        Utilities.computeHmacSha256Signature(text, key);
+        trace.counter ("count256", {args:{count:i,random:Math.random()*LOOPSIZE}});
+      }
+      
+      
+    trace.end('hmac256');
+    
+    trace.begin ('hmac512');
+    
+      for (var i = 0 ; i < LOOPSIZE ; i++) {
+        Utilities.computeHmacSignature(Utilities.MacAlgorithm.HMAC_SHA_512, text, key);
+        trace.counter ("count512", {args:{count:i,random:Math.random()*LOOPSIZE}});
+      }
+      
+    trace.end('hmac512');
+
+  trace.end('hmac');
+
+  trace.dump("/Published Scripts/tracing");
+  
+  
+}
+
